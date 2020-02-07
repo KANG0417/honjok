682b3bc02eefcf363e6b75586c5f0f3595467bf5
KANG0417 <57513472+KANG0417@users.noreply.github.com>
2020-02-07 12:57:10 +0900

    ss

---
+++ honjok/src/main/java/com/honjok/app/login.java
@@ -0,0 +1,17 @@
+package com.honjok.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class login {
+
+	@RequestMapping(value="/login.do", method=RequestMethod.POST)
+	public String login() {
+		System.out.println(">>> 로그인 처리 - login()");
+		
+		//일부러 예외발생
+		return null;
+	}		
+}
