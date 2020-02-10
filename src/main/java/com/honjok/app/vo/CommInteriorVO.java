69a1a4d2e6c31318be1696489d4f66429cee8461
KANG0417 <57513472+KANG0417@users.noreply.github.com>
2020-02-10 18:02:20 +0900

    fgdfg

--- src/main/java/com/honjok/app/vo/CommInteriorVO.java
+++ src/main/java/com/honjok/app/vo/CommInteriorVO.java
@@ -1,5 +1,56 @@
 package com.honjok.app.vo;
 
 public class CommInteriorVO {
-	private String erd;
+	private String type, file_image;
+	private int com_seq, p_num;
+	
+	
+	public CommInteriorVO() {
+	}
+
+	public CommInteriorVO(String type, String file_image, int com_seq, int p_num) {
+		super();
+		this.type = type;
+		this.file_image = file_image;
+		this.com_seq = com_seq;
+		this.p_num = p_num;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getFile_image() {
+		return file_image;
+	}
+
+	public void setFile_image(String file_image) {
+		this.file_image = file_image;
+	}
+
+	public int getCom_seq() {
+		return com_seq;
+	}
+
+	public void setCom_seq(int com_seq) {
+		this.com_seq = com_seq;
+	}
+
+	public int getP_num() {
+		return p_num;
+	}
+
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
+	}
+
+	@Override
+	public String toString() {
+		return "CommInteriorVO [type=" + type + ", file_image=" + file_image + ", com_seq=" + com_seq + ", p_num="
+				+ p_num + "]";
+	}
 }
