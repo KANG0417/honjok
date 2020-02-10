579b2fe3a122a1aa27535a266424f1d4aceca7ba
leesoonyong <wnghkdcjdtns@naver.com>
2020-02-10 11:21:37 +0900

    Create UserDAO.java

---
+++ src/main/java/com/honjok/app/user/impl/UserDAO.java
@@ -0,0 +1,19 @@
+package com.honjok.app.user.impl;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.honjok.app.UserVO;
+
+@Repository("UserDAO")
+public class UserDAO {
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public void insertBoard(UserVO vo) {
+		System.out.println("회원가입 vo를 데이터베이스로 전송중");
+		mybatis.insert("User.insertUser", vo);
+	}
+}
