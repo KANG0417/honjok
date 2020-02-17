ee5b27534a5433555e594a85965a1b4d768707c2
KANG0417 <57513472+KANG0417@users.noreply.github.com>
2020-02-17 14:53:00 +0900

    dsfsd

--- src/main/java/com/honjok/app/cook/CookService.java
+++ src/main/java/com/honjok/app/cook/CookService.java
@@ -3,21 +3,13 @@ package com.honjok.app.cook;
 import java.util.List;
 import java.util.Map;
 
+import com.honjok.app.vo.CommunityVO;
 import com.honjok.app.vo.CookVO;
 
 public interface CookService {
-	
-	  public void insertCook(CookVO cvo);
 
-	  public List<CookVO> selectAll(Map<String, Integer> pagingMap);
+	   public CommunityVO select(String com_seq);
 
-	   public CookVO select(String com_seq);
-
-	   public void delete(String com_seq);
-
-	   public void update(CookVO com);
-	   
-	   public int selectAllCount();
 	    
 	   
 }
--- src/main/java/com/honjok/app/cook/CookServiceImpl.java
+++ src/main/java/com/honjok/app/cook/CookServiceImpl.java
@@ -4,52 +4,28 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.honjok.app.vo.CommunityVO;
 import com.honjok.app.vo.CookVO;
 
+
+@Service
 public class CookServiceImpl implements CookService{
 	
 	@Autowired
 	private CookMapper Mapper;
 	
+	
+	
 	@Override
-	public void insertCook(CookVO cvo) {
-		System.out.println("전");
-		Mapper.insertCook(cvo);
-		System.out.println("후");
-	}
-
-	@Override
-	public List<CookVO> selectAll(Map<String, Integer> pagingMap) {
-		List<CookVO> list = Mapper.selectAll(pagingMap);
-		return list;
-	}
-
-	@Override
-	public CookVO select(String com_seq) {
+	public CommunityVO select(String com_seq) {
 		// TODO Auto-generated method stub
-		CookVO cvo = Mapper.select(com_seq);
+		CommunityVO cvo = Mapper.select(com_seq);
 		return cvo;
 	}
 
-	@Override
-	public void delete(String com_seq) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void update(CookVO cvo) {
-		// TODO Auto-generated method stub
-		Mapper.update(cvo);
-	}
-
-	@Override
-	public int selectAllCount() {
-		// TODO Auto-generated method stub
-		return Mapper.selectAllCount();
-	}
-
+	
 	
 
 }
