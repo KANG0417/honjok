f99cbd0cd2aacafb71fa024d02729b5453e29ffa
KANG0417 <57513472+KANG0417@users.noreply.github.com>
2020-02-19 12:38:25 +0900

    rr

--- src/main/java/com/honjok/app/interior/impl/interiorController.java
+++ src/main/java/com/honjok/app/interior/impl/interiorController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -61,8 +62,12 @@ public class interiorController {
 		return "interiorAllList.do";
 	}
 	
-	/*@RequestMapping("/updateInterior")
-	public String updateBoard(CommInteriorVO cvo) {
+	@RequestMapping("/updateInterior")
+	public String updateBoard(@ModelAttribute("board") CommInteriorVO cvo) {
+		System.out.println(">>> 글 수정 처리 - updateBoard()");
+		System.out.println("> board vo : " + cvo);
 		
-	}*/
+		interiorService.updateBoard(cvo);
+		return "interiorAllList.do";
+	}
 }
