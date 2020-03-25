602971520b8e6387991315742fe599477d802725
smhj1003 <57647004+smhj1003@users.noreply.github.com>
2020-03-25 14:25:38 +0900

    sin

--- src/main/webapp/resources/js/ProductDetail/ProductDetail.js
+++ src/main/webapp/resources/js/ProductDetail/ProductDetail.js
@@ -150,192 +150,197 @@ function reviewForm(e) {
 
 
 //리뷰 페이징 
-$(document).on("click",".production-review__paginator", function(e){
-	if(e.target.nodeName != "BUTTON") return;
+$(document).on("click", ".production-review__paginator", function (e) {
+	if (e.target.nodeName != "BUTTON") return;
 	console.log(e.target.innerText);
-	
+
 	var cPage = e.target.innerText;
-	console.log( cPage);
-	console.log( $('input[name="pNum"]').val());
+	console.log(cPage);
+	console.log($('input[name="pNum"]').val());
 
 	$.ajax({
-				type : 'post',
-				url : "ProductReview.do",
-				data : {
-					cPage : cPage,
-					pNum : $('input[name="pNum"]').val()
-				},
-				success : function(e) {
-					console.log(e);
-			
+		type: 'post',
+		url: "ProductReview.do",
+		data: {
+			cPage: cPage,
+			pNum: $('input[name="pNum"]').val()
+		},
+		success: function (e) {
+			console.log(e);
+
 			$('.production-review-item__container').html("");
-			
-			for(var j = 0; j < e.productreviewvo.length; j++){
-			
-			//작성자 이름 생성  <p>
-				var writer =  '<p class="production-review-item__writer__info__name">작성자: '+e.productreviewvo[j].id+'</p>';
-			//별점 생성
+
+			for (var j = 0; j < e.productreviewvo.length; j++) {
+
+				//작성자 이름 생성  <p>
+				var writer = '<p class="production-review-item__writer__info__name">작성자: ' + e.productreviewvo[j].id + '</p>';
+				//별점 생성
 				var ratings = document.createElement('SPAN');
 				$(ratings).addClass('production-review-item__writer__info__total-star');
-			for(var i = 0; i < e.productreviewvo[j].rating; i++){
-				$(ratings).append('<i class="rating__icon rating__icon--star fa fa-star"></i>');
-			}
-			// 작성일자 
-				var regdate =  '<span class="production-review-item__writer__info__date">'+e.productreviewvo[j].regdate+'</span>';
+				for (var i = 0; i < e.productreviewvo[j].rating; i++) {
+					$(ratings).append('<i class="rating__icon rating__icon--star fa fa-star"></i>');
+				}
+				// 작성일자 
+				var regdate = '<span class="production-review-item__writer__info__date">' + e.productreviewvo[j].regdate + '</span>';
 				var writerInfo = document.createElement('DIV');
 				$(writerInfo).addClass('production-review-item__writer__info');
-				
-				$(writerInfo).append(writer+$(ratings).html()+regdate);
+
+				$(writerInfo).append(writer + $(ratings).html() + regdate);
 
 				var article = document.createElement('ARTICLE');
 				$(article).addClass('production-review-item');
-				
+
 				article.append(writerInfo);
-				$(article).append('<div>'+e.productreviewvo[j].title+'</div>');
-				$(article).append('<img src="/app/resources/img/review/'+e.productreviewvo[j].photoImage1+'"style="width: 120px; height: 100px">');
-				$(article).append('<img src="/app/resources/img/review/'+e.productreviewvo[j].photoImage2+'"style="width: 120px; height: 100px">');
-				$(article).append('<p class="production-review-item__description">내용: <br>'+e.productreviewvo[j].content+'</p>');
+				$(article).append('<div>' + e.productreviewvo[j].title + '</div>');
+				$(article).append('<img src="/app/resources/img/review/' + e.productreviewvo[j].photoImage1 + '"style="width: 120px; height: 100px">');
+				$(article).append('<img src="/app/resources/img/review/' + e.productreviewvo[j].photoImage2 + '"style="width: 120px; height: 100px">');
+				$(article).append('<p class="production-review-item__description">내용: <br>' + e.productreviewvo[j].content + '</p>');
 				$('.production-review-item__container').append(article);
 
 
 				console.log(article);
 			}
-			
-			
+
+
 			var perBtn = "";
-				console.log(e.p.beginPage);
-				if(e.p.beginPage != 1){
-					perBtn  = '<li><button class="list-paginator__prev" type="button">이전</button></li>';
-				}
-				console.log(perBtn);
-				
-				
-
-				var liTag = "";
-				for(var i = e.p.beginPage ; i <= e.p.endPage; i++ ){
-					liTag += '<li style="margin-right:10px"><button class="list-paginator__page sm selected" type="button">'+i+'</button>';
-	                   
-				}
-				console.log(liTag);
-				
-				
-				var nextBtn = "";
-				if(e.p.endPage < e.p.totalPage){
-					var nextBtn  = '<li><button class="list-paginator__next" type="button">다음</button></li>';
-				}
-				console.log(nextBtn);
-				
-				
-			$('.production-review-item__container').append('<ul class="list-paginator production-review__paginator">'+perBtn+liTag+nextBtn+'</ul>');
-				
+			console.log(e.p.beginPage);
+			if (e.p.beginPage != 1) {
+				perBtn = '<li style="padding: 1px 6px;"><button class="list-paginator__prev" type="button">이전</button></li>';
+			}
 			console.log(perBtn);
-			
 
-				},
-				error : function(jqXHR, textStatus, errorThrown) {
-					alert("오류가 발생하였습니다.");
-				}
 
-			});
+
+			var liTag = "";
+			for (var i = e.p.beginPage; i <= e.p.endPage; i++) {
+				liTag += '<li style="padding: 1px 6px;"><button class="list-paginator__page sm selected" type="button">' + i + '</button>';
+
+			}
+			console.log(liTag);
+
+
+			var nextBtn = "";
+			if (e.p.endPage < e.p.totalPage) {
+				var nextBtn = '<li style="padding: 1px 6px;"><button class="list-paginator__next" type="button">다음</button></li>';
+			}
+			console.log(nextBtn);
+
+
+			$('.production-review-item__container').append('<ul class="list-paginator production-review__paginator">' + perBtn + liTag + nextBtn + '</ul>');
+
+			console.log(perBtn);
+
+
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			alert("오류가 발생하였습니다.");
+		}
+
+	});
 
 
 });
 
 
-$("#product-select").on("change", function(){
-	
+$("#product-select").on("change", function () {
+
 	var price = document.querySelector('.price');
 	var pNum = document.querySelector('.production-selling-header__title__name');
 
 	$('#_optionSelectList').append('<li style="padding: 20px;">\
-									<div class="prdprice_left"><span class="prd_txtbox">'+pNum.innerHTML+'</span>\
-									</div><div><div class="btns"><button >감소</button><input class="ctn"  type="number"  value="1" dir="rtl"><button>증가</button> \
-									<strong><em class="total-price">'+price.innerHTML+'</em>원</strong><button>취소</button></div></div></li>');
-	
-	
+									<div class="prdprice_left"><span class="prd_txtbox">'+ pNum.innerHTML + '</span>\
+									</div><div><div class="btns"><input type="button" value="감소"><input class="ctn" type="number"  value="1" dir="rtl"><input type="button" value="증가"> \
+									<strong><em class="total-price">'+ price.innerHTML + '</em>원</strong><input type="button" value="취소"></div></div></li>');
+
+
 	purchasePrice = $('.total-price').parent().find('em').html();
 	$('.selling-option-form-content__price__number').html(purchasePrice);
 
 });
 
-	$(document).on("change",".ctn",function(e){
-			var stock = $('.stock').text();
-			if( parseInt(stock) < e.target.value ){
+$(document).on("change", ".ctn", function (e) {
+	var stock = $('.stock').text();
+	if (parseInt(stock) < e.target.value) {
 
-				alert("재고가 부족합니다.");
-				e.target.value = 1;
-				return;
-			}
+		alert("재고가 부족합니다.");
+		e.target.value = 1;
+		return;
+	}
 
-			if( 1 > e.target.value ){
+	if (1 > e.target.value) {
 
-				alert("1개보다 작을수 없습니다.");
-				e.target.value = 1;
-				return;
-			}
+		alert("1개보다 작을수 없습니다.");
+		e.target.value = 1;
+		return;
+	}
 
-			var price = $(e.target).parent().find('em').html();
-			price = parseInt(purchasePrice) * e.target.value;
-			
-			$(e.target).parent().find('em').html(price);
-			$('.selling-option-form-content__price__number').html(price);
-			$('input[name="pCnt"]').val(price);
-			console.log(price);
+	var price = $(e.target).parent().find('em').html();
+	price = parseInt(purchasePrice) * e.target.value;
 
-
+	$(e.target).parent().find('em').html(price);
+	$('.selling-option-form-content__price__number').html(price);
+	$('input[name="pCnt"]').val(e.target.value);
+	console.log(price);
 
 
 
 
 
 
-	})
 
-	$(document).on("click",".btns button",function(e){
-		console.log(e.target.innerText);
+
+})
+
+$(document).on("click", ".btns input", function (e) {
+	console.log(e.target.innerText);
+	var ctn = $('.ctn').val();
+	if (e.target.value == '감소') {
+
+		if (ctn <= 1) { alert("1보다 작을수 없습니다"); return }
+		ctn--;
+		$('.ctn').val(ctn);
+
+		var price = $(e.target).parent().find('em').html();
+		price = parseInt(price) - parseInt(purchasePrice);
+
+		$(e.target).parent().find('em').html(price);
+		$('.selling-option-form-content__price__number').html(price);
+		$('input[name="pCnt"]').val(ctn);
+		console.log(price);
+
+	} else if (e.target.value == '증가') {
+
+		var stock = $('.stock').text();
+		console.log(stock);
+		if (ctn >= parseInt(stock)) { alert("재고가 부족합니다"); return }
+
 		var ctn = $('.ctn').val();
-		if(e.target.innerText == '감소'){
+		ctn++;
+		$('.ctn').val(ctn);
 
-			if(ctn <= 1 ) {alert("1보다 작을수 없습니다");return}
-			ctn--;
-			$('.ctn').val(ctn);
-
-			var price = $(e.target).parent().find('em').html();
-			price = parseInt(price) - parseInt(purchasePrice);
-			
-			$(e.target).parent().find('em').html(price);
-			$('.selling-option-form-content__price__number').html(price);
-			$('input[name="pCnt"]').val(price);
-			console.log(price);
-
-			}else if(e.target.innerText == '증가'){
-
-			var stock = $('.stock').text();
-			console.log(stock);
-			if(ctn >= parseInt(stock)) {alert("재고가 부족합니다");return}
-			
-			var ctn = $('.ctn').val();
-				ctn++;
-				$('.ctn').val(ctn);
-
-			var price = $(e.target).parent().find('em').html();
-			price = parseInt(price) +parseInt(purchasePrice);
-			$(e.target).parent().find('em').html(price);
-			$('.selling-option-form-content__price__number').html(price);
-			$('input[name="pCnt"]').val(price);
+		var price = $(e.target).parent().find('em').html();
+		price = parseInt(price) + parseInt(purchasePrice);
+		$(e.target).parent().find('em').html(price);
+		$('.selling-option-form-content__price__number').html(price);
+		$('input[name="pCnt"]').val(ctn);
 
 
-		}else{
+	} else {
 
-			var del = $(e.target).parent().parent().parent();
-			console.log(del);
-			
-			$(del).remove();
-			$('.selling-option-form-content__price__number').html("0");
+		var del = $(e.target).parent().parent().parent();
+		console.log(del);
+
+		$(del).remove();
+		$('.selling-option-form-content__price__number').html("0");
 
 
-		}
-	})
+	}
+})
+
+
+
+
+
 
 
 
